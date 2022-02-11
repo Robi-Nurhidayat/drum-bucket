bc6770fc854f0d8d966f4f2a6f44196e2c15c4f8
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-02-11 18:05:45 +0700

    add a eventlister

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("test")
+let numberOfDrum = document.querySelectorAll('.drum').length;
+
+for(let i = 0; i < numberOfDrum; i++){
+    document.querySelectorAll('.drum')[i].addEventListener('click',function(){
+        alert('hey you click me!')
+    })
+}
