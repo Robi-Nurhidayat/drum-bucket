fecea39bc283dd0b75b858e57b682f618427be5a
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-02-14 16:39:57 +0700

    learn this keyword

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let numberOfDrum = document.querySelectorAll('.drum').length;
 
 for(let i = 0; i < numberOfDrum; i++){
     document.querySelectorAll('.drum')[i].addEventListener('click',function(){
-        alert('hey you click me!')
+        // var temp = `sounds/tom-${i+1}.mp3`;
+        // console.log(temp)
+        // var audio = new Audio(temp);
+        // audio.play();
+
+        this.style.color="white";
     })
 }
