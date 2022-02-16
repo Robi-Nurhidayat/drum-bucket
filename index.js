ae589142aabb2796b5711db4e3e6e9be6ad7940c
Robi Nurhidayat <robivanpersie290899@gmail.com>
2022-02-17 05:41:20 +0700

    learn switch

--- index.js
+++ index.js
@@ -7,6 +7,40 @@ for(let i = 0; i < numberOfDrum; i++){
         // var audio = new Audio(temp);
         // audio.play();
 
-        this.style.color="white";
+        var buttonAudio = this.innerHTML;
+
+        switch(buttonAudio){
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play()
+            break;
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play()
+            break;
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play()
+            break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play()
+            break;
+            case "j":
+                var tom1 = new Audio('sounds/snare.mp3');
+                tom1.play()
+            break;
+            case "k":
+                var tom1 = new Audio('sounds/crash.mp3');
+                tom1.play()
+            break;
+            case "l":
+                var tom1 = new Audio('sounds/kick-bass.mp3');
+                tom1.play()
+            break;
+            default:
+                console.log("noting")
+            
+        }
     })
 }
